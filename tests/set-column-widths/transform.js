9f8f0af8053721c6a11205d34c20da56ac17826a
Kelly Joseph Price <kelly@collaborativedrug.com>
2017-05-18 11:44:36 -0700

    Address eslint

--- tests/set-column-widths/transform.js
+++ tests/set-column-widths/transform.js
@@ -1,5 +1,5 @@
 module.exports = function(plugin, state) {
-    let transform = state.transform();
+    const transform = state.transform();
 
     const cursorBlock = state.document.getDescendant('_cursor');
     state = transform.moveToRangeOf(cursorBlock).apply();
