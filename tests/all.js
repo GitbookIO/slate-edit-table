5aa8a16ddfb79d1f955a39d102434fd3dfb73486
Soreine <soreine.plume@gmail.com>
2018-06-28 12:23:14 +0200

    Fix support of selection in hyperscript

--- tests/all.js
+++ tests/all.js
@@ -1,52 +1,50 @@
 /* eslint-disable global-require */
 /* eslint-disable import/no-dynamic-require */
-import expect from 'expect';
-import fs from 'fs';
-import path from 'path';
-import Slate from 'slate';
-import hyperprint from 'slate-hyperprint';
-import EditTable from '../lib';
+import expect from "expect";
+import fs from "fs";
+import path from "path";
+import Slate from "slate";
+import hyperprint from "slate-hyperprint";
+import EditTable from "../lib";
 
 const PLUGIN = EditTable();
 const SCHEMA = Slate.Schema.create({
-    plugins: [PLUGIN]
+  plugins: [PLUGIN]
 });
 
 function deserializeValue(value) {
-    return Slate.Value.fromJSON(
-        { document: value.document, schema: SCHEMA },
-        { normalize: false }
-    );
+  return Slate.Value.fromJSON(
+    { document: value.document, schema: SCHEMA, selection: value.selection },
+    { normalize: false }
+  );
 }
 
-describe('slate-edit-table', () => {
-    const tests = fs.readdirSync(__dirname);
+describe("slate-edit-table", () => {
+  const tests = fs.readdirSync(__dirname);
 
-    tests.forEach(test => {
-        if (test[0] === '.' || path.extname(test).length > 0) return;
+  tests.forEach(test => {
+    if (test[0] === "." || path.extname(test).length > 0) return;
 
-        it(test, () => {
-            Slate.resetKeyGenerator();
-            const dir = path.resolve(__dirname, test);
-            const input = require(path.resolve(dir, 'input.js')).default;
-            const expectedPath = path.resolve(dir, 'expected.js');
-            const expected =
-                fs.existsSync(expectedPath) && require(expectedPath).default;
+    it(test, () => {
+      Slate.resetKeyGenerator();
+      const dir = path.resolve(__dirname, test);
+      const input = require(path.resolve(dir, "input.js")).default;
+      const expectedPath = path.resolve(dir, "expected.js");
+      const expected =
+        fs.existsSync(expectedPath) && require(expectedPath).default;
 
-            const runChange = require(path.resolve(dir, 'change.js')).default;
+      const runChange = require(path.resolve(dir, "change.js")).default;
 
-            const valueInput = deserializeValue(input);
+      const valueInput = deserializeValue(input);
 
-            const newChange = runChange(PLUGIN, valueInput.change());
+      const newChange = runChange(PLUGIN, valueInput.change());
 
-            if (expected) {
-                const newDoc = hyperprint(newChange.value.document, {
-                    strict: true
-                });
-                expect(newDoc).toEqual(
-                    hyperprint(expected.document, { strict: true })
-                );
-            }
+      if (expected) {
+        const newDoc = hyperprint(newChange.value.document, {
+          strict: true
         });
+        expect(newDoc).toEqual(hyperprint(expected.document, { strict: true }));
+      }
     });
+  });
 });
