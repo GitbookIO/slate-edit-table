0b0b3116af84bf3a3de59d1ee1e064fb2352cb10
Soreine <soreine.plume@gmail.com>
2018-08-24 15:03:43 +0200

    Handle onPaste

---
+++ tests/paste-multiple-cells-into-table/change.js
@@ -0,0 +1,19 @@
+import expect from 'expect';
+
+export default function(plugin, change) {
+    const { value } = change;
+
+    // Copy the selection
+    const copiedFragment = plugin.utils.getCopiedFragment(value);
+    // Default copy in this case
+    expect(copiedFragment).toBeTruthy();
+
+    // Paste it
+    change.collapseToAnchor();
+
+    return plugin.changes.insertTableFragmentAtRange(
+        change,
+        change.value.selection,
+        copiedFragment
+    );
+}
