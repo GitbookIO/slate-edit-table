86321742dabfe23f64cedaa597c857b2db87c684
kyrisu <krystian.paszkiewicz@gmail.com>
2018-08-30 15:48:55 +0100

    Updated slate to v0.40.2 and fixed 2 failing tests

--- tests/schema-rows-require-same-columns-counts/change.js
+++ tests/schema-rows-require-same-columns-counts/change.js
@@ -1,5 +1,10 @@
 import Slate from 'slate';
 
+/**
+ * @param plugin
+ * @param {Slate.Change} change
+ * @returns {Slate.Change}
+ */
 export default (plugin, change) => {
     const schema = Slate.Schema.create(plugin.schema);
     return change.normalize(schema);
