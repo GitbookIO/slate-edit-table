ccf1fb05eb703a9e432904ac4050ac32b1258e7f
Soreine <soreine.plume@gmail.com>
2018-09-18 17:43:38 +0200

    Updates

--- value.js
+++ value.js
@@ -1,6 +1,6 @@
 /** @jsx h */
 // eslint-disable-next-line import/no-extraneous-dependencies
-import { createHyperscript } from 'slate-hyperscript';
+import { createHyperscript } from '@gitbook/slate-hyperscript';
 
 const h = createHyperscript({
     blocks: {
