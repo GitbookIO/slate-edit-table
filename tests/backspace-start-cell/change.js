0bab3d878c8f83e1c84d7a425dab2aff7640838f
Gabin <hello@gabinaureche.com>
2019-01-16 16:39:59 +0100

    Do not prevent default behavior when cursor is not at the start of cell

--- tests/backspace-start-cell/change.js
+++ tests/backspace-start-cell/change.js
@@ -3,18 +3,24 @@ import expect from 'expect';
 export default function(plugin, change) {
     const { value } = change;
     const blockStart = value.document.getDescendant('anchor');
-
     const withCursor = change.collapseToStartOf(blockStart);
 
+    let isDefaultPrevented = false;
     const result = plugin.onKeyDown(
         {
             key: 'Backspace',
-            preventDefault() {},
+            preventDefault() {
+                isDefaultPrevented = true;
+            },
             stopPropagation() {}
         },
         withCursor
     );
 
+    // It should have prevented the default behavior...
+    expect(isDefaultPrevented).toBe(true);
+
+    // ...and left the change unchanged
     expect(result).toBe(change);
 
     return change;
