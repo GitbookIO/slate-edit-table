5e2f7059351fbaea9fcaec47fbd3931cd11ad9ed
Gabin <hello@gabinaureche.com>
2019-01-16 16:52:00 +0100

    Ensure that the default behavior is not altered

--- tests/backspace-after-inline/change.js
+++ tests/backspace-after-inline/change.js
@@ -1,15 +1,22 @@
 import expect from 'expect';
 
 export default function(plugin, change) {
+    let isDefaultPrevented = false;
     const result = plugin.onKeyDown(
         {
             key: 'Backspace',
-            preventDefault() {},
+            preventDefault() {
+                isDefaultPrevented = true;
+            },
             stopPropagation() {}
         },
         change
     );
 
+    // It shouldn't alter the default behavior...
+    expect(isDefaultPrevented).toBe(false);
+
+    // ...and let Slate do the work
     expect(result).toBe(undefined);
 
     return change;
